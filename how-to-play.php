8c75901254c79c74b13179bda9517abac245680f
Austin Houck <akh4rf@virginia.edu>
2021-09-27 02:44:41 -0400

    Created other pages to demonstrate routing

---
+++ how-to-play.php
@@ -0,0 +1,7 @@
+<?php include_once "header.php" ?>
+
+<div style="height: 100%; width: 100%; display: flex; justify-content: center; align-items: center; font-size: 64px;">
+  This is the how to play page!
+</div>
+
+<?php include_once "footer.php" ?>
