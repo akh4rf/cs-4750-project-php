e097cc26daf1d91eca34bec253f00008383e2f11
Austin Houck <akh4rf@virginia.edu>
2021-10-05 00:17:10 -0400

    Moved page wrapper styling to external .css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include_once "includes/header.php" ?>
 
-<div style="height: 100%; width: 100%; display: flex; justify-content: center; align-items: center; font-size: 64px;">
+<div class="inner-page-contents">
   This is our homepage! It looks really cool!
 </div>
 
