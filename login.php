3f7f5752c87b57aaf05f22c2d8593447f3d2e5ed
Austin Houck <akh4rf@virginia.edu>
2021-10-13 20:14:30 -0400

    Updated login page styles

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <div class="inner-page-contents">
   <div style="width: 100%;">
     <form class="login-form" action="">
-      <h1 style="font-size: 48px; font-weight: 700; margin-bottom: 40px;">LOG IN</h1>
+      <h1 style="font-size: 48px; font-weight: 700; margin-bottom: 40px; text-shadow: -4px 4px 5px var(--dark-blue);">LOG IN</h1>
       <input type="text" name="username" id="username" autofocus placeholder="Enter Username...">
       <input type="password" name="password" id="password" placeholder="Enter Password...">
       <button type="submit">Submit</button>
