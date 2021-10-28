9fd312d9828b4d8d513936679926ffbdc833a883
Austin Houck <akh4rf@virginia.edu>
2021-10-28 17:27:33 -0400

    Updated Feedback Styling

--- submit-feedback.php
+++ submit-feedback.php
@@ -2,13 +2,21 @@
 
 <link rel="stylesheet" href="css/feedback.css">
 <div class="inner-page-contents">
-  <div style ="width: 50%">
-      <form class="feedback-form" action="">
-        <h1 style="font-size: 40px; font-weight: 700; margin-top: 20px; margin-bottom: 40px; text-shadow: -4px 4px 5px var(--dark-blue);">SUBMIT FEEDBACK</h1>
-        <input type="radio"> 
-        <textarea autofocus placeholder="Enter your review here..."></textarea>
-        <button type="submit">Submit</button>
-      </form>
+  <div class="feedback-container">
+    <div class="feedback-title">
+      <h1>Submit Feedback</h1>
+    </div>
+    <form class="feedback-form" action="">
+      <h2 style="margin-top: 25px; color: var(--dark-blue); font-weight: 700;">How Would You Rate Our App?</h2>
+      <div id="rate" class="rate">
+        <?php for ($i = 5; $i > 0; $i--) : ?>
+          <input type="radio" id="star<?php echo $i ?>" name="rate" value="<?php echo $i ?>" />
+          <label for="star<?php echo $i ?>" title="star<?php echo $i ?>">★</label>
+        <?php endfor ?>
+      </div>
+      <textarea autofocus placeholder="Enter your review here..."></textarea>
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </div>
 
