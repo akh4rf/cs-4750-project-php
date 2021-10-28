c4bad1143838dc2c5c5eb99da680c32d6653d980
Jeffrey Etter <jeffreyetter@Jeffreys-MacBook-Pro.local>
2021-10-28 16:40:26 -0400

    working on feedback page

--- submit-feedback.php
+++ submit-feedback.php
@@ -1,7 +1,15 @@
 <?php include_once "includes/header.php" ?>
 
+<link rel="stylesheet" href="css/feedback.css">
 <div class="inner-page-contents">
-  This is the feedback submission page!
+  <div style ="width: 50%">
+      <form class="feedback-form" action="">
+        <h1 style="font-size: 40px; font-weight: 700; margin-top: 20px; margin-bottom: 40px; text-shadow: -4px 4px 5px var(--dark-blue);">SUBMIT FEEDBACK</h1>
+        <input type="radio"> 
+        <textarea autofocus placeholder="Enter your review here..."></textarea>
+        <button type="submit">Submit</button>
+      </form>
+  </div>
 </div>
 
 <?php include_once "includes/footer.php" ?>
