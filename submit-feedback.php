a6cafa70bef93f3a9d9b0e2a011f59f9e44bca83
Ziyao22 <70288628+Ziyao22@users.noreply.github.com>
2021-11-07 15:02:18 -0500

    Add a title field and adjust the overall style.

--- submit-feedback.php
+++ submit-feedback.php
@@ -1,6 +1,12 @@
 <?php include_once "includes/header.php" ?>
 
 <link rel="stylesheet" href="css/feedback.css">
+<style>
+  p{
+    color: red;
+    padding: 20px;
+  }
+</style>
 <div class="inner-page-contents">
   <div class="feedback-container">
     <div class="feedback-title">
@@ -14,7 +20,9 @@
           <label for="star<?php echo $i ?>" title="star<?php echo $i ?>">★</label>
         <?php endfor ?>
       </div>
+      <textarea autofocus placeholder="Enter your title here..." class="title"  style=" font-size: 100px min-width:500px; max-width:50%;min-height:50px;height:200%;width:50%;"></textarea>
       <textarea autofocus placeholder="Enter your review here..."></textarea>
+      <p>Your feedback will not be shared with anyone else!</p>
       <button type="submit">Submit</button>
     </form>
   </div>
