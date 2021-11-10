d9014a0831fb9e28e705072a76382f56aabb1700
Ziyao22 <70288628+Ziyao22@users.noreply.github.com>
2021-11-09 21:05:38 -0500

    Update submit-feedback.php

--- submit-feedback.php
+++ submit-feedback.php
@@ -10,7 +10,6 @@ include './database/db-helpers.php';
 
 
 //1: get post data:
-session_start();
   if($_SERVER["REQUEST_METHOD"]==='POST'){
     $myuserid=$_POST['UserID'];
     $mytitle=$_POST['title'];
@@ -21,15 +20,15 @@ session_start();
     //"INSERT INTO Users VALUES (NULL, ?, ?)"
     
     $data=execute_query($sql,array($myuserid, $mytimestamp,$mytitle,$mycomment,$myrating));
-    if($data['row_count']==1){
-      $user=$data['rows_affected'][0];
-      $_SESSION['UserID']=$user['UserID'];
-      $_SESSION['timestamp']=$user['timestamp'];
-      $_SESSION['title']=$user['title'];
-      $_SESSION['comment']=$user['comment'];
-      $_SESSION['rating']=$user['rating'];
-      header("location: submit-feedback");
-    }
+    //if($data['row_count']==1){
+     // $user=$data['rows_affected'][0];
+      //$_SESSION['UserID']=$user['UserID'];
+      //$_SESSION['timestamp']=$user['timestamp'];
+      //$_SESSION['title']=$user['title'];
+      //$_SESSION['comment']=$user['comment'];
+      //$_SESSION['rating']=$user['rating'];
+      //header("location: submit-feedback");
+    //}
   }
 ?>
 
@@ -46,15 +45,15 @@ session_start();
       <h1>Submit Feedback</h1>
     </div>
     <form class="feedback-form" action="submit-feedback" method="post">
-      <h2 style="margin-top: 25px; color: var(--dark-blue); font-weight: 700;">How Would You Rate Our App?</h2>
+      <h2 style="margin-top: 25px; color: var(--dark-blue); font-weight: 700;" >How Would You Rate Our App?</h2>
       <div id="rate" class="rate">
         <?php for ($i = 5; $i > 0; $i--) : ?>
           <input type="radio" id="star<?php echo $i ?>" name="rate" value="<?php echo $i ?>" />
           <label for="star<?php echo $i ?>" title="star<?php echo $i ?>">★</label>
         <?php endfor ?>
       </div>
-        <input type="text" placeholder="Enter your title here..." class="title"  style="margin-bottom: 25px; width: 30%; height: 50px;" autofocus>
-      <textarea placeholder="Enter your review here..."></textarea>
+      <input type="text" placeholder="Enter your title here..." class="title"  style="margin-bottom: 25px; width: 30%; height: 50px;" autofocus  required>
+      <textarea placeholder="Enter your review here..." ></textarea>
       <p>Your feedback will not be shared with anyone else!</p>
       <button type="submit">Submit</button>
     </form>
