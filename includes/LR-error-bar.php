1d91e216af9055a7928e3a2f9104c15ae83ab09d
Lychee030 <61603017+Lychee030@users.noreply.github.com>
2021-11-09 22:13:13 -0500

    changed the error bar a little bit

--- includes/LR-error-bar.php
+++ includes/LR-error-bar.php
@@ -3,7 +3,7 @@
 function LR_error_bar($message)
 {
   echo "<div class='login-error-bar'>
-          <p style='color:white; font-size: 2em;'>" . $message . "</p>
+          <p style='color:white; font-size: 1em;'>" . $message . "</p>
           <i class='fas fa-times login-error-dismiss'></i>
         </div>
         ";
