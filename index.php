883f2f6ce8ea809e943c7e45f92cd0caadb26322
Lychee030 <61603017+Lychee030@users.noreply.github.com>
2021-11-15 09:31:40 -0500

    styling issue fixed

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 <link rel="stylesheet" href="css/home.css">
 <div class="inner-page-contents">
-  <div class="title-container">
-    <h1 class="home-title">WELCOME TO UPPER90</h1>
-  </div>
   <div class="homeImage-container">
     <?php echo "<img src='images/homePageDesign.png'>"?>
   </div>
