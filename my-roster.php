c877946fbf5b657918eb301f1bf1bcbf4ff36323
Austin Houck <akh4rf@virginia.edu>
2021-11-14 20:58:40 -0500

    Fixed null image issue and enabled remove player

--- my-roster.php
+++ my-roster.php
@@ -11,18 +11,18 @@ session_start(); //did session start so i could see changes take place, but in r
 // $data=execute_query($sql,array($myuserid)); //updates database
 
 //Team Information
-$myuserid='100001'; //hardcoded, needs to be gained from session later
-$sql="SELECT name, description, homeColor, awayColor, nationality FROM Team WHERE userid=?;";
-$teaminfo = execute_query($sql,array($myuserid));
-if($teaminfo['row_count']==1){
-  $user=$teaminfo['rows_affected'][0];
-    $teamname=$user['name'];
-    $description=$user['description'];
-    $homeColor=$user['homeColor'];
-    $awayColor=$user['awayColor'];
-    $nationality=$user['nationality'];
-}else{
-  $error_msg="Error!";
+$myuserid = '100001'; //hardcoded, needs to be gained from session later
+$sql = "SELECT name, description, homeColor, awayColor, nationality FROM Team WHERE userid=?;";
+$teaminfo = execute_query($sql, array($myuserid));
+if ($teaminfo['row_count'] == 1) {
+  $user = $teaminfo['rows_affected'][0];
+  $teamname = $user['name'];
+  $description = $user['description'];
+  $homeColor = $user['homeColor'];
+  $awayColor = $user['awayColor'];
+  $nationality = $user['nationality'];
+} else {
+  $error_msg = "Error!";
 }
 
 //Roster
@@ -30,10 +30,23 @@ if($teaminfo['row_count']==1){
 //TeamPlayer RLPID, TeamID
 //Team TeamID, UserID(input)
 
-$sql2="SELECT name, picURL, age, position, mvps, goals, assists FROM RLPlayer NATURAL JOIN TeamPlayer WHERE TeamID=?";
-$teamid=2;//teamID=2 is example
-$rosterinfo = execute_query($sql2,array($teamid));
-$rostersize=$rosterinfo['row_count'];
+$sql2 = "SELECT RLPID, name, picURL, age, position, mvps, goals, assists FROM RLPlayer NATURAL JOIN TeamPlayer WHERE TeamID=?";
+$teamid = 2; //teamID=2 is example
+$rosterinfo = execute_query($sql2, array($teamid));
+$rostersize = $rosterinfo['row_count'];
+
+for ($i = 0; $i < $rostersize; $i++) {
+  if (isset($_POST['button-' . $i])) {
+    $sql3 = "DELETE FROM TeamPlayer WHERE TeamID=? AND RLPID=?";
+    $player = $rosterinfo['rows_affected'][$i]['RLPID'];
+    // Delete player from team in DB
+    $removeinfo = execute_query($sql3, array($teamid, $player));
+    // Decrement roster size and nullify player in returned team data
+    $rostersize -= 1;
+    $rosterinfo['rows_affected'][$i] = NULL;
+  }
+}
+
 ?>
 
 
@@ -72,63 +85,65 @@ $rostersize=$rosterinfo['row_count'];
         <h1>ROSTER</h1>
         <hr style="margin-top: 10px;">
       </div>
-      <div class="roster">
+      <form action="my-roster" method="post" class="roster">
         <?php for ($i = 0; $i < $rostersize; $i++) :
-            $info=$rosterinfo['rows_affected'][$i];
+          $info = $rosterinfo['rows_affected'][$i];
         ?>
           <div class="roster-player-wrapper">
             <div class="roster-player-info">
               <div style="height: 100%; display: flex; padding: 0 20px;">
                 <div style="height: 100%; display: flex; flex-direction: column; justify-content: center; font-weight: 700;">
-                  <div><?php echo $info['name']?></div>
+                  <div><?php echo $info['name'] ?></div>
+                  <?php if ($info['age'] > 0) : ?>
                   <div>Age: <?php echo $info['age'] ?></div>
+                  <?php endif; ?>
                   <div><?php echo substr($info['position'], 0, strlen($info['position']) - 1) ?></div>
                 </div>
                 <div style="display: flex; margin: 0 auto;">
                   <div class="roster-player-statbox">
-                  <div><?php echo $info['goals'] ?></div>
+                    <div><?php echo $info['goals'] ?></div>
                     <div>Goals</div>
                   </div>
                   <div class="roster-player-statbox" style="margin: 0 25px">
-                  <div><?php echo $info['assists'] ?></div>
+                    <div><?php echo $info['assists'] ?></div>
                     <div>Assists</div>
                   </div>
                   <div class="roster-player-statbox">
-                  <div><?php echo $info['mvps'] ?></div>
+                    <div><?php echo $info['mvps'] ?></div>
                     <div>MVPs</div>
                   </div>
                 </div>
               </div>
-              <button id=<?php echo $i ?> onclick="removePlayer(this.id)"><i class="far fa-times-circle" style="display: flex;"></i></button>
-              <div class="roster-player-pfp"> 
-                <img src=<?php echo $info['picURL'] ?> alt="">
+              <button type="submit" name="button-<?php echo $i ?>"><i class="far fa-times-circle" style="display: flex;"></i></button>
+              <div class="roster-player-pfp">
+                <?php if ($info['picURL']) : ?>
+                  <img src=<?php echo $info['picURL'] ?> alt="">
+                <?php else : ?>
+                  <p style="color: white; font-weight: 700;">No Picture</p>
+                <?php endif; ?>
               </div>
             </div>
           </div>
         <?php endfor ?>
         <?php for ($i = $rostersize; $i < 5; $i++) :
         ?>
-        <div class="roster-player-wrapper">
+          <div class="roster-player-wrapper">
             <div class="roster-player-info">
-              <div style="height: 100%; display: flex; padding: 0 20px;">
-                <div>ADD A PLAYER</div>
+              <div style="height: 100%; display: flex; justify-content: center; align-items: center; padding: 0 20px;">
+                <div style="font-weight: 700;">EMPTY PLAYER SLOT</div>
               </div>
-              <button id=<?php echo $i ?> onclick="removePlayer(this.id)"><i class="far fa-times-circle" style="display: flex;"></i></button>
+              <button type="submit" name="button-<?php echo $i ?>"><i class="far fa-times-circle" style="display: flex;"></i></button>
               <div class="roster-player-pfp">
-                <img src="Messi.png" alt="">
+                <a style="border-radius: 999px; height: 60%; width: 60%; border: 5px solid white; display: flex; justify-content: center; align-items: center; text-decoration: none;" href="<?php echo transformPath('/player-search'); ?>">
+                  <i class="fas fa-plus" style="color: white; font-size: 48px;"></i>
+                </a>
               </div>
             </div>
-        </div>
+          </div>
         <?php endfor ?>
-      </div>
+      </form>
     </div>
   </div>
 </div>
-
-<script>
-  function removePlayer(num) {
-    console.log(num);
-  }
-</script>
 
 <?php include_once "includes/footer.php" ?>
