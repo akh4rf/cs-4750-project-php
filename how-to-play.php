afc683b4d849a3cbf658de012c05d430652532ea
Ziyao22 <70288628+Ziyao22@users.noreply.github.com>
2021-11-17 17:15:57 -0500

    Update how-to-play.php

--- how-to-play.php
+++ how-to-play.php
@@ -1,7 +1,8 @@
 <?php include_once "includes/header.php" ?>
+<h1 style="color: blue; text-align:center;"> Instructions to get started play the game. </h1>
 
 <div class="inner-page-contents">
-  This is the how to play page!
+  <p class="Instructions">Figure shows how to register and login to get started to play the game:</p>
+  <img src="https://static01.nyt.com/images/2020/09/25/sports/25soccer-nationalWEB1/merlin_177451008_91c7b66d-3c8a-4963-896e-54280f374b6d-superJumbo.jpg"  width="700" height="700">
 </div>
-
 <?php include_once "includes/footer.php" ?>
