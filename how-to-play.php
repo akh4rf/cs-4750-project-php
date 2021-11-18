b936e0129aa206408a6d550479db0fdd904f34f1
Aayush Malde <malde.aayush@gmail.com>
2021-11-17 20:31:41 -0500

    layout for howtoplay page

--- how-to-play.php
+++ how-to-play.php
@@ -1,8 +1,25 @@
 <?php include_once "includes/header.php" ?>
-<h1 style="color: blue; text-align:center;"> Instructions to get started play the game. </h1>
+<link rel="stylesheet" href="css/instructions.css">
+
+<div class="htpheader">
+<h1 style="font-size: 40px; color: navy"> How to Play </h1>
+</div>
+
 
 <div class="inner-page-contents">
+
+<div class="backgroundinfo">
+  <p>Welcome to Upper90! You are in charge of a fantasy soccer team of five players.</p>
+  <p>The goal is to choose players and assemble a roster that gets you the most points. </p>
+  <p>You can compete with your friends and make changes to your team along the way.</p>
+</div>
+
+<div class="scoring">
+  <p>Each player's points is based on their real life performances</p>
+  <p>The point breakdown is as follows:</p>
+</div>
+
   <p class="Instructions">Figure shows how to register and login to get started to play the game:</p>
-  <img src="https://static01.nyt.com/images/2020/09/25/sports/25soccer-nationalWEB1/merlin_177451008_91c7b66d-3c8a-4963-896e-54280f374b6d-superJumbo.jpg"  width="700" height="700">
+  <img src="https://static01.nyt.com/images/2020/09/25/sports/25soccer-nationalWEB1/merlin_177451008_91c7b66d-3c8a-4963-896e-54280f374b6d-superJumbo.jpg"  width="100" height="100">
 </div>
 <?php include_once "includes/footer.php" ?>
