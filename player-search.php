97460baa47f1afe755f4013dd143198b72fb39a4
Austin Houck <akh4rf@virginia.edu>
2021-11-27 05:09:32 -0500

    Minor pagination fixes & url rewriting

--- player-search.php
+++ player-search.php
@@ -6,8 +6,6 @@ include './database/db-helpers.php';
 
 loginCheck();
 
-
-
 $teamid = execute_query("SELECT TeamID FROM Team WHERE UserID=?", array($_SESSION['UserID']))['rows_affected'][0]['TeamID'];
 
 // Current Team Members
@@ -55,15 +53,31 @@ if (isset($_POST['POST-TYPE'])) {
       }
     }
   } else if ($_POST['POST-TYPE'] == 'Search') {
-    // Update results to match search terms
-    $searchInput = "%" . $_POST['player-search'] . "%";
-    $sql .= ' WHERE name LIKE ?';
-    array_push($search_params, $searchInput);
+    $_SESSION['player-search'] = $_POST['player-search'];
   }
 }
 
+// Clears search terms if redirecting from another page
+if (isset($_SERVER['HTTP_REFERER'])) {
+  if (!strpos($_SERVER['HTTP_REFERER'], 'player-search')) {
+    unset($_SESSION['player-search']);
+    unset($_SESSION['sort']);
+  }
+}
+
+if (isset($_SESSION['player-search'])) {
+  // Update results to match search terms
+  $searchInput = "%" . $_SESSION['player-search'] . "%";
+  $sql .= ' WHERE name LIKE ?';
+  array_push($search_params, $searchInput);
+}
+
 if (isset($_POST['sort'])) {
-  switch ($_POST['sort']) {
+  $_SESSION['sort'] = $_POST['sort'];
+}
+
+if (isset($_SESSION['sort'])) {
+  switch ($_SESSION['sort']) {
     case 'Name_A_Z':
       // Sort by ascending name
       $sql .= " ORDER BY name";
@@ -89,19 +103,16 @@ if (isset($_POST['sort'])) {
   }
 }
 
+
 $pageno = 1;
 if (isset($_GET['pageno'])) {
-  $pageno = $_GET['pageno'];
+  $pageno = intval($_GET['pageno']);
 }
 $no_of_records_per_page = 10;
-$offset = ($pageno-1) * $no_of_records_per_page;
-$total_pages_sql = "SELECT COUNT(*) FROM RLPlayer";
+$offset = ($pageno - 1) * $no_of_records_per_page;
 
-$total_page = execute_query($total_pages_sql, array());
-//print_r($total_page['row_count']);
-$total_pages = 1710/$no_of_records_per_page;
+$total_pages = execute_query($sql, $search_params)['row_count'] / $no_of_records_per_page;
 $sql .= " LIMIT $offset, $no_of_records_per_page";
-
 $data = execute_query($sql, $search_params);
 
 function actionsTD($RLPID)
@@ -144,10 +155,10 @@ function textTD($contents)
 
 ?>
 
-<link rel="stylesheet" href="css/player-search.css">
+<link rel="stylesheet" href=<?php echo transformPath('/css/player-search.css') ?>>
 <div class="inner-page-contents">
   <div class="player-search">
-    <form class="player-search-top-section" action="player-search" method="post">
+    <form class="player-search-top-section" action="" method="post">
       <div class="ps-input">
         <input type="text" name="POST-TYPE" value="Search" style="display: none;">
         <input type="text" name="player-search" id="player-search" placeholder="Search Player Info..." autofocus>
@@ -156,19 +167,41 @@ function textTD($contents)
       <div class="ps-dropdown">
         <label for="sort">Sort By: </label>
         <select name="sort" id="sort-input" onchange="this.form.submit()">
-          <option <?php if(! isset($_POST['sort'])){echo 'selected';} ?>disabled value="">----- Select Option -----</option>
-          <option <?php if(isset($_POST['sort'])) {if($_POST['sort']=='Name_A_Z'){echo 'selected';}} ?> value="Name_A_Z">Name (A-Z)</option>
-          <option <?php if(isset($_POST['sort'])) {if($_POST['sort']=='Name_Z_A'){echo 'selected';}} ?> value="Name_Z_A">Name (Z-A)</option>
-          <option <?php if(isset($_POST['sort'])) {if($_POST['sort']=='Goals'){echo 'selected';}} ?> value="Goals">Goals</option>
-          <option <?php if(isset($_POST['sort'])) {if($_POST['sort']=='Assists'){echo 'selected';}} ?> value="Assists">Assists</option>
-          <option <?php if(isset($_POST['sort'])) {if($_POST['sort']=='MVPs'){echo 'selected';}} ?> value="MVPs">MVPs</option>
+          <option <?php if (!isset($_SESSION['sort'])) {
+                    echo 'selected';
+                  } ?>disabled value="">----- Select Option -----</option>
+          <option <?php if (isset($_SESSION['sort'])) {
+                    if ($_SESSION['sort'] == 'Name_A_Z') {
+                      echo 'selected';
+                    }
+                  } ?> value="Name_A_Z">Name (A-Z)</option>
+          <option <?php if (isset($_SESSION['sort'])) {
+                    if ($_SESSION['sort'] == 'Name_Z_A') {
+                      echo 'selected';
+                    }
+                  } ?> value="Name_Z_A">Name (Z-A)</option>
+          <option <?php if (isset($_SESSION['sort'])) {
+                    if ($_SESSION['sort'] == 'Goals') {
+                      echo 'selected';
+                    }
+                  } ?> value="Goals">Goals</option>
+          <option <?php if (isset($_SESSION['sort'])) {
+                    if ($_SESSION['sort'] == 'Assists') {
+                      echo 'selected';
+                    }
+                  } ?> value="Assists">Assists</option>
+          <option <?php if (isset($_SESSION['sort'])) {
+                    if ($_SESSION['sort'] == 'MVPs') {
+                      echo 'selected';
+                    }
+                  } ?> value="MVPs">MVPs</option>
         </select>
       </div>
       <input type="submit" style="display: none" />
     </form>
 
     <div class="player-search-bottom-section">
-      <form class="table-wrapper" action="player-search" method="post">
+      <form class="table-wrapper" action="" method="post">
         <input type="text" name="POST-TYPE" value="Add/Remove" style="display: none;">
         <table>
           <thead>
@@ -210,14 +243,26 @@ function textTD($contents)
     </div>
   </div>
   <ul class="pagination">
-      <li><a href="?pageno=1">First</a></li>
-      <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-        <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Previous</a>
-      </li>
-      <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-        <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
-      </li>
-      <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+    <li><a href=<?php echo transformPath('/player-search/page/1') ?>>First</a></li>
+    <li class="<?php if ($pageno <= 1) {
+                  echo 'disabled';
+                } ?>">
+      <a href=<?php if ($pageno <= 1) {
+                  echo '#';
+                } else {
+                  echo transformPath('/player-search/page/'. ($pageno - 1));
+                } ?>>Previous</a>
+    </li>
+    <li class="<?php if ($pageno >= $total_pages) {
+                  echo 'disabled';
+                } ?>">
+      <a href="<?php if ($pageno >= $total_pages) {
+                  echo '#';
+                } else {
+                  echo transformPath('/player-search/page/' . ($pageno + 1));
+                } ?>">Next</a>
+    </li>
+    <li><a href="?pageno=<?php echo transformPath('/player-search/page/' . $total_pages); ?>">Last</a></li>
   </ul>
 </div>
 
