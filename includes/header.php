55e3b24362c148676d5087f239b5a417b23066a6
Aayush Malde <malde.aayush@gmail.com>
2021-11-29 19:27:31 -0500

    shows profile pic topright

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,22 @@
 
 <?php include_once 'url-helpers.php';
 include 'login-check.php';
-session_start(); ?>
+session_start(); 
+
+loginCheck();
+
+include("./database/db-helpers.php");
+$myuserid = $_SESSION['UserID'];
+$sql = "SELECT profilePicURL FROM UserInfo WHERE UserID=?;";
+$data = execute_query($sql, array($myuserid));
+if ($data['row_count'] == 1) {
+  $user = $data['rows_affected'][0];
+  $profilePicURL = $user['profilePicURL'];
+} else {
+  $error_msg = "Error with UserInfo";
+}
+
+?>
 
 <head>
   <meta charset="UTF-8">
@@ -22,7 +37,7 @@ session_start(); ?>
       <a style="height: 100%; text-decoration: none; padding: 0 25px; color: white;" href=<?php echo transformPath('/') ?>>
         <div style="display: flex; align-items: center; height: 100%; font-size: 32px;">Upper90</div>
       </a>
-      <div style="font-size: 45px; padding: 0 25px;"><a style="text-decoration: none; color: white; height: 100%; display: flex; align-items: center;" href=<?php echo transformPath('/login') ?>><i class="far fa-user-circle"></i></a></div>
+      <div style="font-size: 45px; padding: 0 25px;"><a style="text-decoration: none; color: white; height: 100%; display: flex; align-items: center;" href=<?php echo transformPath('/login') ?>><img src=<?php echo $profilePicURL ?> style="width:50px;height: 50px;"></a></div>
     </div>
     <div style="height: 100%; width: 100%; position: relative;">
       <?php include_once "includes/sidebar.php" ?>
